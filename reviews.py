99427745181632d49cea1766a33072eed2a8a331
Your Name <you@example.com>
2025-08-24 14:36:36 +0900

    refactor of reviews

--- reviews.py
+++ reviews.py
@@ -1,53 +1,28 @@
-"""Module that provides top-level ProjNet Dr Checks reviews XML parsing functionality"""
+""" 
+This module defines the abstract and concrete class used for deserializing
+conversation building blocks (Comements, Evaluations and Backchecks) from 
+ProjNet DrChecks XML reports.
+"""
 
-from typing import List, Dict, Tuple
+from abc import ABC, abstractmethod
+from datetime import datetime
+from heapq import merge
 from defusedxml import ElementTree as ET
-# from utils import parse_single_tag
-# from remarks import Comment
-
-from blocks import Comment, parse_single_tag
 
 _PROJECT_INFO_INDEX = 0
 _COMMENTS_INDEX = 1
-_COMMENT_COLUMNS = ['id', 
-                    'status', 
-                    'discipline',
-                    'author',
-                    '',
-                    'date_created',
-                    'text',
-                    'is_critical',
-                    '',
-                    'has_attachment']
-
-_COMMENTS_COLUMNS = {
-    'ID': 'id',
-    'Status': 'status',
-    'Discipline': 'discipline',
-    'Author': 'author',
-    'Email': 'email',
-    'Date': 'date_created',
-    'Comment': 'text',
-    'Critical': 'is_critical',
-    'Class': '',
-    'Att.': ''
-}
-
-
-
-
-
-
 
 def get_root(path):
     try:
         root = ET.parse(path).getroot()
         if root.tag.lower() == 'projnet': # type: ignore
             return root
+        else:
+            return None
     except Exception as e:
         print(e)
 
-def get_review(root):
+def get_review_elements(root):
     """Returns a tuple of the element nodes for project info and all comments"""
     return (root[_PROJECT_INFO_INDEX], root[_COMMENTS_INDEX])
 
@@ -65,6 +40,34 @@ def _get_review_comments_element(root):
     except Exception as e:
         print(e)
 
+def parse_single_tag(tag, xml_element_node):
+    """Helper method to extract XML data for first child element node with tag of tag."""
+    return xml_element_node.find(tag).text if xml_element_node.find(tag) is not None else None
+
+def clean_text(text):
+    return text.replace('<br />', '\n')
+
+
+class Review():
+
+    def __init__(self,
+                 project_info=None,
+                 review_comments=None,
+                 root=None):
+        self.project_info = project_info
+        self.review_comments = review_comments
+        self.root = root
+
+    @classmethod
+    def from_file(cls, path):
+        root = get_root(path)
+        if root:
+            project_info = root[_PROJECT_INFO_INDEX]
+            review_comments = root[_COMMENTS_INDEX]
+        return Review(project_info=project_info,
+                      review_comments=review_comments,
+                      root=root)
+
 
 class ProjectInfo():
     
@@ -74,11 +77,11 @@ class ProjectInfo():
                  project_name,
                  review_id,
                  review_name):
-        self._project_id = project_id
-        self._control_number = control_number
-        self._project_name = project_name
-        self._review_id = review_id
-        self._review_name = review_name
+        self.project_id = project_id
+        self.control_number = control_number
+        self.project_name = project_name
+        self.review_id = review_id
+        self.review_name = review_name
 
     @classmethod
     def from_tree(cls, element):
@@ -92,43 +95,23 @@ class ProjectInfo():
                            project_name=project_name,
                            review_id=review_id,
                            review_name=review_name)
-
-    @property
-    def project_id(self):
-        return self._project_id
-    
-    @property
-    def control_number(self):
-        return self._control_number
-    
-    @property
-    def project_name(self):
-        return self._project_name
-    
-    @property
-    def review_id(self):
-        return self._review_id
-    
-    @property
-    def review_name(self):
-        return self._review_name
-    
+   
     @property
     def all_data(self):
         return {
-            'Project Name': self._project_name,
-            'Project ID': self._project_id,
-            'Control Number': self._control_number,
-            'Review Name': self._review_name,
-            'Review ID': self._review_id
+            'Project Name': self.project_name,
+            'Project ID': self.project_id,
+            'Control Number': self.control_number,
+            'Review Name': self.review_name,
+            'Review ID': self.review_id
         }
 
 
 class ReviewComments():
 
     def __init__(self,
-                 comments: List=[]):
-        self._comments = comments
+                 comments=[]):
+        self.comments = comments
 
     @classmethod
     def from_tree(cls, element):
@@ -138,12 +121,8 @@ class ReviewComments():
         return ReviewComments(comments=comments)
     
     @property
-    def comments(self):
-        return self._comments
-
-    @property
     def comment_count(self):
-        return len(self._comments)
+        return len(self.comments)
 
     @property
     def max_evaulations_count(self):
@@ -183,4 +162,222 @@ class ReviewComments():
     @property
     def total_responses_count(self):
         return self.total_evaulations_count + self.total_backchecks_count
+
+
+class Remark(ABC):
+    """Parent class for Comment, Evaluation and Backcheck classes"""
+    def __init__(self,
+                 id_=None,
+                 status=None,
+                 text=None,
+                 has_attachment=None,
+                 author=None,
+                 date_created=None,
+                 remark_type='remark'):
+        self.id = id_
+        self.status = status
+        self.text = text
+        self.has_attachment = has_attachment
+        self.author = author
+        self.date_created = date_created
+        self.remark_type = remark_type
+
+    @property
+    def attributes_list(self):
+        return [attr for attr in self.__dict__]
+
+    @abstractmethod
+    def from_tree(self):
+        pass
+
+    @abstractmethod
+    def dump_values(self):
+        pass
+
+
+class Comment(Remark):
     
+    def __init__(self, 
+                 id_=None, 
+                 status=None, 
+                 text=None, 
+                 has_attachment=None, 
+                 author=None, 
+                 date_created=None, 
+                 remark_type='comment',
+                 spec=None,
+                 sheet=None,
+                 detail=None,
+                 is_critical=None,
+                 docref=None,
+                 doctype=None,
+                 discipline=None,
+                 coordinating_discipline=None,
+                 evaluations=[],
+                 backchecks=[]):
+        super().__init__(id_, status, text, has_attachment, author, date_created, remark_type)
+        self.spec = spec
+        self.sheet = sheet
+        self.detail = detail
+        self.is_critical = is_critical
+        self.docref = docref
+        self.doctype = doctype
+        self.discipline = discipline
+        self.coordinating_discipline = coordinating_discipline
+        self.evaluations = evaluations
+        self.backchecks = backchecks
+
+    @classmethod
+    def from_tree(cls, element):
+        id_ = parse_single_tag('id', element)
+        spec = parse_single_tag('spec', element)
+        sheet = parse_single_tag('sheet', element)
+        detail = parse_single_tag('detail', element)
+        is_critical = parse_single_tag('critical', element)
+        docref = parse_single_tag('DocRef', element)
+        doctype = parse_single_tag('DocType', element)
+        discipline = parse_single_tag('Discipline', element)
+        coordinating_discipline = parse_single_tag('CoordinatingDiscipline', element)     
+        status = parse_single_tag('status', element)
+        text = clean_text(parse_single_tag('commentText', element))
+        has_attachment = True if element.find('attachment') is not None else False
+        author = parse_single_tag('createdBy', element)
+        date_created = datetime.strptime(element.find('createdOn').text, 
+                                        '%b %d %Y %I:%M %p').isoformat() if \
+                                            element.find('createdOn') is not None else None,
+        evaluations = [Evaluation.from_tree(eval) for eval in element.find('evaluations')] \
+            if element.find('evaluations') is not None else []
+        backchecks = [Backcheck.from_tree(bc) for bc in element.find('backchecks')] \
+                    if element.find('backchecks') is not None else []
+        return Comment(id_=id_,
+                    spec=spec,
+                    sheet=sheet,
+                    detail=detail,
+                    is_critical=is_critical,
+                    docref=docref,
+                    doctype=doctype,
+                    discipline=discipline,
+                    coordinating_discipline=coordinating_discipline,
+                    status=status,
+                    text=text,
+                    has_attachment=has_attachment,
+                    author=author,
+                    date_created=date_created,
+                    evaluations=evaluations,
+                    backchecks=backchecks)
+
+    def dump_values(self):
+        raise NotImplementedError
+
+    @property
+    def evaluations_count(self):
+        return len(self.evaluations)
+
+    @property
+    def backchecks_count(self):
+        return len(self.backchecks)
+    
+    @property
+    def response_count(self):
+        return len(self.evaluations) + len(self.backchecks)
+    
+    @property
+    def list_reponses(self):
+        """Returns list of Responses in type order: first Evaluations then Backchecks."""
+        return self.evaluations + self.backchecks
+
+    @property
+    def list_responses_chronological(self):
+        """Returns list of Responses in chronological order, regardless of Evaluation or Backcheck type."""
+        sort_key = lambda Remark: Remark.date_created
+        return list(merge(self.evaluations, self.backchecks, key=sort_key))
+
+
+class Evaluation(Remark):
+
+    def __init__(self, 
+                 id_=None, 
+                 status=None, 
+                 text=None, 
+                 has_attachment=None, 
+                 author=None, 
+                 date_created=None, 
+                 remark_type='evaluation',
+                 parent_id=None,
+                 impact_scope=None,
+                 impact_cost=None,
+                 impact_time=None):
+        super().__init__(id_, status, text, has_attachment, author, date_created, remark_type)
+        self._parent_id = parent_id
+        self._impact_scope = impact_scope
+        self._impact_cost = impact_cost
+        self._impact_time = impact_time
+
+    @classmethod
+    def from_tree(cls, element):
+        id_ = parse_single_tag('id', element)
+        parent_id = parse_single_tag('comment', element)
+        status = parse_single_tag('status', element)
+        impact_scope = parse_single_tag('impactScope', element)
+        impact_cost = parse_single_tag('impactCost', element)
+        impact_time = parse_single_tag('impactTime', element)
+        text = clean_text(parse_single_tag('evaluationText', element))
+        has_attachment = True if element.find('attachment').text is not None else False
+        author = parse_single_tag('createdBy', element)
+        date_created = datetime.strptime(element.find('createdOn').text, 
+                                        '%b %d %Y %I:%M %p').isoformat() if \
+                                            element.find('createdOn') is not None else None
+        return Evaluation(id_=id_, 
+                        parent_id=parent_id, 
+                        status=status, 
+                        impact_scope=impact_scope,
+                        impact_cost=impact_cost,
+                        impact_time=impact_time,
+                        text=text,
+                        has_attachment=has_attachment,
+                        author=author,
+                        date_created=date_created)
+
+    def dump_values(self):
+        raise NotImplementedError
+
+
+class Backcheck(Remark):
+
+    def __init__(self, 
+                 id_=None, 
+                 status=None, 
+                 text=None, 
+                 has_attachment=None, 
+                 author=None, 
+                 date_created=None, 
+                 remark_type='backcheck',
+                 parent_id=None,
+                 evaluation_id=None):
+        super().__init__(id_, status, text, has_attachment, author, date_created, remark_type)
+        self._parent_id = parent_id
+        self._evaulation_id = evaluation_id
+
+    @classmethod
+    def from_tree(cls, element):
+        id_ = parse_single_tag('id', element)
+        parent_id = parse_single_tag('comment', element)
+        evaluation_id = parse_single_tag('evaluation', element)
+        status = parse_single_tag('status', element)
+        text = clean_text(parse_single_tag('backcheckText', element))
+        has_attachment = True if element.find('attachment').text is not None else False
+        author = parse_single_tag('createdBy', element)
+        date_created = datetime.strptime(element.find('createdOn').text, 
+                                        '%b %d %Y %I:%M %p').isoformat() if \
+                                            element.find('createdOn') is not None else None
+        return Backcheck(id_=id_, 
+                        parent_id=parent_id, 
+                        status=status, 
+                        evaluation_id=evaluation_id,
+                        text=text,
+                        has_attachment=has_attachment,
+                        author=author,
+                        date_created=date_created)
+    
+    def dump_values(self):
+        raise NotImplementedError
