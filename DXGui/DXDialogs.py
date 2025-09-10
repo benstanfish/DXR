f23f8309130529695dd6379e232ea150b03f8ee1
Ben Fisher <benstanfish@hotmail.com>
2025-09-10 09:20:54 +0900

    .

--- DXGui/DXDialogs.py
+++ DXGui/DXDialogs.py
@@ -1,26 +1,77 @@
+import sys, os
+from typing import List, Tuple, Dict, Any, Literal
+from PyQt6.QtWidgets import QFileDialog, QMessageBox
+from PyQt6.QtGui import QIcon
 
-from PyQt6.Widgets import 
+_DIALOG_TYPES = Literal['critical', 'warning', 'info', 'question', 'none']
 
-class SelectFileDialog:
+class SelectFileDialog(QFileDialog):
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+    def get_files(self,
+                  caption='Select Files Dialog',
+                  filter='XML Files (*.xml);;HTML Files (*.html);;All Files (*)'):
+        self.paths, _ = self.getOpenFileNames(None, caption=caption, filter=filter)
+        return self.paths
+
+
+class SelectFolderDialog():
     pass
 
-class SelectFolderDialog:
+
+class SaveAsDialog(QFileDialog):
     pass
 
-class SaveDialog:
-    pass
 
-class AlertDialog:
-    pass
+class InfoDialog(QMessageBox):
+    
+    def __init__(self):
+        super().__init__()
 
-class InfoDialog:
-    pass
+    def show(self, 
+             type:_DIALOG_TYPES='info',
+             title='Dialog Window',
+             text='Success!',
+             buttons=QMessageBox.StandardButton.Ok | QMessageBox.StandardButton.Cancel):
+        self.dialog = QMessageBox(self)
+        self.setWindowIcon(QIcon('./rsx/img/icon.ico'))
+        self.setWindowTitle(title)
+        self.setText(text)
+        self.setStandardButtons(buttons)
+        if type=='critical':
+            self.setIcon(QMessageBox.Icon.Critical)
+        elif type=='warning':
+            self.setIcon(QMessageBox.Icon.Warning)
+        elif type=='info':
+            self.setIcon(QMessageBox.Icon.Information)
+        elif type=='question':
+            self.setIcon(QMessageBox.Icon.Question)
+        elif type=='none':
+            self.setIcon(QMessageBox.Icon.NoIcon)
+        self.exec()
 
-class SuccessDialog:
-    pass
 
 class AboutDialog:
     pass
 
+
 class TroubleDialog:
     pass
+
+
+
+class _TestApplication():
+    from PyQt6.QtWidgets import QApplication
+    test_app = QApplication([])
+
+    select_dialog = SelectFileDialog()
+    paths = select_dialog.get_files(caption='Please select report XML files')
+    print(paths)
+
+    # alert = AlertDialog()
+    # info_dialog = InfoDialog()
+    # info_dialog.show(type='none')
+
+    test_app.quit()
