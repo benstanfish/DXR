c1a3163980c8c5c99fc6e8232cbb13334c098f7d
Ben Fisher <benstanfish@hotmail.com>
2025-09-10 09:52:18 +0900

    .

---
+++ __init__.py
@@ -0,0 +1,11 @@
+# Copyright (c) 2018-2025 Ben Fisher
+
+from dxbuild import Review
+import _constants as constants
+
+__author__ = constants.__author__
+__author_email__ = constants.__author_email__
+__license__ = constants.__license__
+__maintainer_email__ = constants.__maintainer_email__
+__url__ = constants.__url__
+__version__ = constants.__version__
---
+++ _constants.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2018-2025 Ben Fisher
+
+"""
+Package metadata
+"""
+
+__author__ = "Ben Fisher"
+__author_email__ = "benstanfish@gmail.com"
+__license__ = "GPL-3"
+__maintainer_email__ = __author_email__
+__url__ = ''
+__version__ = "2.0"
+__python__ = "3.13"
