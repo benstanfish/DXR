7621bc5df575ea3a50de50e96ae97e65b7aa2f2d
Your Name <you@example.com>
2025-09-21 09:03:21 +0900

    .

--- unit_xml.py
+++ unit_xml.py
@@ -1,6 +1,7 @@
 # Copyright (c) 2018-2025 Ben Fisher
 
 import xml.etree.ElementTree as ET
+import random
 from faker import Faker
 from dxbuild.buildtools import timestamp
 from typing import Literal
@@ -22,18 +23,29 @@ proj_info_dict = {'ProjectID': fake.numerify('######'),
 for item in proj_info_dict:
     ET.SubElement(proj_info, item).text = proj_info_dict[item]
 
-statuses = Literal['Closed', 'Closed without comment.', 'Non-Concur']
-backcheck_dict = {'id': fake.numerify('######'),
-                  'comment': '1',
-                  'evaluation': '1',
-                  'status': 'Concur',
-                  'backcheckText': fake.text(100),
-                  'attachment': fake.boolean(),
-                  'createdBy': fake.name(),
-                  'createdOn': fake.date(date_format_string)}
 
-for item in backcheck_dict:
-    ET.SubElement(review_comments, item).text = backcheck_dict[item]
+
+
+
+
+
+def make_backcheck(parent_element):
+    backcheck_statuses = ['Closed', 'Closed without comment.', 'Non-Concur']
+    backcheck_dict = {'id': fake.numerify('######'),
+                    'comment': '1',
+                    'evaluation': '1',
+                    'status': random.choice(backcheck_statuses),
+                    'backcheckText': fake.text(100),
+                    'attachment': random.choice(['True', '']),
+                    'createdBy': fake.name(),
+                    'createdOn': fake.date(date_format_string)}
+    for item in backcheck_dict:
+        ET.SubElement(parent_element, item).text = backcheck_dict[item]
+
+for i in range(1, 10):
+    bc = ET.SubElement(review_comments, f'backcheck{i}')
+    make_backcheck(bc)
+
 
 file_name = f'./dev/test/xml/test_xml_{timestamp()}.xml'
 tree = ET.ElementTree(root)
