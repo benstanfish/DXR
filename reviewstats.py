f3243026795b18fd1a55e23b6378d628db53b2f1
Ben Fisher <benstanfish@hotmail.com>
2025-10-10 14:33:50 +0900

    .

--- reviewstats.py
+++ reviewstats.py
@@ -1,10 +1,12 @@
 # Copyright (c) 2018-2025 Ben Fisher
 
 import sys, os
+# import win32com.client as COM
 
 from openpyxl import Workbook
 from openpyxl.styles import DEFAULT_FONT
-from openpyxl.worksheet.formula import ArrayFormula
+from openpyxl.worksheet.cell_range import CellRange
+
 
 
 from PyQt6.QtWidgets import QApplication, QFileDialog
@@ -36,39 +38,91 @@ def main() -> None:
     DEFAULT_FONT.__init__(name=FALLBACKS['font_name'], size=FALLBACKS['font_size'])
     wb = Workbook()
 
-    # app = QApplication(sys.argv)
-    # xml_paths, _ = QFileDialog.getOpenFileNames(
-    #     parent=None, 
-    #     caption='Select Files Dialog',
-    #     filter='XML Files (*.xml);;HTML Files (*.html);;All Files (*)'
-    # )
 
     review = Review.from_file(xml_path)
+    comments = review.review_comments.comments
+
     ws = wb.active
-    
     singlereport.create_report(review, ws)
 
-    ws_table_name = ws.tables.items()[0][0]
 
+
+    ws_table_name = ws.tables.items()[0][0]
     ws_stat_name = ws.title[:25] + '-STAT'
     ws_stats = wb.create_sheet(ws_stat_name)
     
-    comments = review.review_comments.comments
     disciplines = list(set([comment.discipline for comment in comments]))
-    for disc in disciplines:
-        print(disc, len(disciplines))
-
-    ws_stats['A1'].value = f'=UNIQUE({ws_table_name}[Discipline])'
+    reviewers = list(set([comment.author for comment in comments]))
 
 
+    # for disc in disciplines:
+    #     print(disc, len(disciplines))
+
+    anchor = ws_stats[CellRange('A1').coord]
+
+    anchor.value = 'Dr Checks Review Statistics'
+    anchor.offset(row=2).value = 'Project Name'
+    anchor.offset(row=3).value = 'Project ID'
+    anchor.offset(row=4).value = 'Review Name'
+
+    anchor.offset(row=2, column=1).value = review.project_info.project_name
+    anchor.offset(row=3, column=1).value = review.project_info.project_id
+    anchor.offset(row=4, column=1).value = review.project_info.review_name
+
+    anchor.offset(row=6).value = 'Overall Comment Status'
+    anchor.offset(row=7).value = 'By Discipline'
+    anchor.offset(row=7, column=1).value = 'Open'
+    anchor.offset(row=7, column=2).value = 'Closed'
+    anchor.offset(row=7, column=3).value = 'Total'
+
+    anchor.offset(row=8).value = f'=UNIQUE({ws_table_name}[Discipline])'
+    anchor.offset(row=8, column=1).value = f'=COUNTIFS({ws_table_name}[Discipline],$A9,{ws_table_name}[Status],B$8)'
+    
+    print(anchor.offset(row=8, column=1).coordinate)
+
+    
+    anchor.offset(row=8 + len(disciplines)).value = 'Grand Total'
 
 
     if _WRITE_FILE:
         save_name = f'./dev/test/out/test_{timestamp()}.xlsx'
         wb.save(save_name)
+        print(f'File {save_name} written to disk.')
         logger.debug(f'_WRITE_FILE = {_WRITE_FILE} -> saved workbook to "{save_name}"')
-    wb.close()
 
+    wb.close()
+    
+    # try:
+    #     excel = COM.Dispatch('Excel.Application')
+    #     excel.Visible = False
+
+    #     workbook = excel.Workbooks.Open(os.path.abspath(save_name))
+    #     sht = workbook.Worksheets(ws_stat_name)
+
+    #     sht.Range('A8').value = 'Discipline'
+    #     sht.Range('B8').value = 'Open'
+    #     sht.Range('C8').value = 'Closed'
+    #     sht.Range('D9').value = 'Total'
+    #     sht.Range('A9').formula2 = f'=UNIQUE({ws_table_name}[Discipline])'
+    #     sht.Range(f'B9:B{9 + len(disciplines) - 1}').formula2 = f'=COUNTIFS({ws_table_name}[Discipline],$A9,{ws_table_name}[Status],B$8)'
+    #     sht.Range(f'C9:C{9 + len(disciplines) - 1}').formula2 = f'=COUNTIFS({ws_table_name}[Discipline],$A9,{ws_table_name}[Status],C$8)'
+    #     sht.Range(f'D9:D{9 + len(disciplines) - 1}').formula2 = f'=AGGREGATE(9,4,B9:C9)'
+    #     sht.Range(f'A{9 + len(disciplines)}').value = 'Grand Total'
+
+    #     sht.Range('F9').formula2 = f'=UNIQUE({ws_table_name}[Author])'
+
+
+
+    # except Exception as e:
+    #     logger.exception(f'Error in trying to edit via COM. {e}')
+    # finally:
+
+    #     workbook.Save()
+    #     workbook.Close()
+    
+    #     if 'excel' in locals() and excel:
+    #         excel.Quit()
+    #     print('Code Executed -> Returned 0')
 
 if __name__ == '__main__':
     main()
